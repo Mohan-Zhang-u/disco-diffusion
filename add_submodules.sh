44bb2934ac5b9835ac875b5375ce0afe8478b1fa
Mohan Zhang <mohanzhang@cs.toronto.edu>
2022-06-12 06:14:15 -0400

    sub modules

---
+++ add_submodules.sh
@@ -0,0 +1,6 @@
+git submodule add git@github.com:Mohan-Zhang-u/CLIP.git
+git submodule add git@github.com:Mohan-Zhang-u/guided-diffusion.git
+git submodule add git@github.com:Mohan-Zhang-u/ResizeRight.git
+git submodule add git@github.com:Mohan-Zhang-u/pytorch3d-lite.git
+git submodule add git@github.com:Mohan-Zhang-u/MiDaS.git
+git submodule add git@github.com:Mohan-Zhang-u/AdaBins.git
